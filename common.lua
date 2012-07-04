7f0492815ba327ad8166712d5b17246faa3fed3c
James Callahan <james@chatid.com>
2012-07-04 17:21:21 +1000

    common: include might as well just be a function

--- common.lua
+++ common.lua
@@ -1,6 +1,4 @@
-include = setmetatable ( {} , {
-	__call = function  ( include , lib )
-		lib = "fend.include." .. lib:gsub(".h$",""):gsub("/",".")
-		return require ( lib )
-	end
-}) -- Is the table modules go into, and the function that pulls them in
+include = function  ( lib )
+	lib = "fend.include." .. lib:gsub(".h$",""):gsub("/",".")
+	return require ( lib )
+end

5bc5b74ceb3b6acd65533c37b5c46c0470f0d834
James Callahan <james@chatid.com>
2012-10-07 08:59:45 +1100

    include/sys: Move fd_set from types to select

--- include/sys/select.lua
+++ include/sys/select.lua
@@ -1,4 +1,14 @@
+include "time"
+
 ffi.cdef [[
+typedef __suseconds_t suseconds_t;
+typedef long int __fd_mask;
+typedef struct
+  {
+    __fd_mask __fds_bits[1024 / (8 * (int) sizeof (__fd_mask))];
+  } fd_set;
+typedef __fd_mask fd_mask;
+
 extern int select (int __nfds, fd_set *__restrict __readfds,
      fd_set *__restrict __writefds,
      fd_set *__restrict __exceptfds,
--- include/sys/types.lua
+++ include/sys/types.lua
@@ -109,23 +109,6 @@ struct timeval
     __time_t tv_sec;
     __suseconds_t tv_usec;
   };
-typedef __suseconds_t suseconds_t;
-typedef long int __fd_mask;
-typedef struct
-  {
-    __fd_mask __fds_bits[1024 / (8 * (int) sizeof (__fd_mask))];
-  } fd_set;
-typedef __fd_mask fd_mask;
-
-extern int select (int __nfds, fd_set *__restrict __readfds,
-     fd_set *__restrict __writefds,
-     fd_set *__restrict __exceptfds,
-     struct timeval *__restrict __timeout);
-extern int pselect (int __nfds, fd_set *__restrict __readfds,
-      fd_set *__restrict __writefds,
-      fd_set *__restrict __exceptfds,
-      const struct timespec *__restrict __timeout,
-      const __sigset_t *__restrict __sigmask);
 
 
 __extension__
